2d1bf688910daaf95b5e3c9a86249f4c82c7bad8
Adriely Nara <a.freitas.barbosa@avanade.com>
2019-06-12 21:03:39 -0300

    Configuration mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
- 
+
+mongoose.connect('mongodb+srv://semanaomnistack:XXX@cluster0-uuxjm.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true, 
+});
+
 app.get('/', (req, res) => {
     return res.send(`Hello ${req.query.name}`);
 });
