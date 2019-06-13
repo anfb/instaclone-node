9ab3489b69cd6a6c15ec426dcd3abf5c9ab278e1
Adriely Nara <a.freitas.barbosa@avanade.com>
2019-06-12 21:19:06 -0300

    configurating routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb+srv://semanaomnistack:XXX@cluster0-uuxjm.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://semanaomnistack:semana@cluster0-uuxjm.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true, 
 });
 
-app.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}`);
-});
+app.use(require('./routes'));
+
 app.listen(3333);
